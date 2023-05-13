921ce72d17f1d9c1621a1aced322d18a519ced71
L-Hyun <dkh1001@outlook.kr>
2023-05-13 15:07:18 +0900

    Feat Add UserStore for manage states like pk of user, tokens

---
+++ src/stores/UserStore.ts
@@ -0,0 +1,39 @@
+import { create } from "zustand";
+
+interface UserInfo {
+	isLoggedIn: boolean;
+	email: string;
+	id: number;
+	accessToken: string;
+	refreshToken: string;
+
+	setIsLoggedIn: (val: boolean) => void;
+	setEmail: (val: string) => void;
+	setId: (val: number) => void;
+	setAccessToken: (val: string) => void;
+	setRefreshToken: (val: string) => void;
+}
+
+export default create<UserInfo>((set) => ({
+	isLoggedIn: false,
+	email: "",
+	id: -1,
+	accessToken: "",
+	refreshToken: "",
+
+	setIsLoggedIn: (val: boolean) => {
+		set({ isLoggedIn: val });
+	},
+	setEmail: (val: string) => {
+		set({ email: val });
+	},
+	setId: (val: number) => {
+		set({ id: val });
+	},
+	setAccessToken: (val: string) => {
+		set({ email: val });
+	},
+	setRefreshToken: (val: string) => {
+		set({ email: val });
+	},
+}));
