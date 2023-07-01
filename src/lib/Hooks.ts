9867b79f422ffc583bc59a042e160d2ea184d815
L-Hyun <dkh1001@outlook.kr>
2023-07-01 17:49:45 +0900

    Feat JWT 파싱 함수 추가

--- src/lib/Hooks.ts
+++ src/lib/Hooks.ts
@@ -1,27 +1,27 @@
 import axios from "axios";
 
-const parseJwt = (token: string) => {
-	let base64Url = token.split(".")[1];
-	let base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
-	let jsonPayload = decodeURIComponent(
-		window
-			.atob(base64)
-			.split("")
-			.map(function (c) {
-				return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
-			})
-			.join("")
-	);
-
-	return JSON.parse(jsonPayload);
-};
-
 const instance = axios.create({
 	baseURL: "http://43.201.192.197:8080/api",
 	timeout: 3000,
 });
 
 export default {
+	parseJwt: (token: string) => {
+		let base64Url = token.split(".")[1];
+		let base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+		let jsonPayload = decodeURIComponent(
+			window
+				.atob(base64)
+				.split("")
+				.map(function (c) {
+					return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+				})
+				.join("")
+		);
+
+		return JSON.parse(jsonPayload);
+	},
+
 	login: async (email: string, pw: string) => {
 		return await instance
 			.post("/member/login", {
