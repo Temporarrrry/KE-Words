4eb8097e57269b63ed7a40dad70f99725a4e9d4e
L-Hyun <dkh1001@outlook.kr>
2023-07-01 17:48:14 +0900

    Feat API 추가

--- src/lib/Hooks.ts
+++ src/lib/Hooks.ts
@@ -17,44 +17,91 @@ const parseJwt = (token: string) => {
 };
 
 const instance = axios.create({
-	baseURL: "http://43.201.192.197:8080",
+	baseURL: "http://43.201.192.197:8080/api",
 	timeout: 3000,
 });
 
 export default {
 	login: async (email: string, pw: string) => {
-		const response = await instance
-			.post("/api/member/login", {
+		return await instance
+			.post("/member/login", {
 				userEmail: email,
 				password: pw,
-			})
-			.then((res) => {
-				return res;
 			})
 			.catch((e) => {
 				console.log("login error", e.response);
 				return e.response;
 			});
-		return response;
 	},
 	register: async (email: string, pw: string) => {
-		const response = await instance
-			.post("/api/member/register", {
+		return await instance
+			.post("/member/register", {
 				userEmail: email,
 				password: pw,
-			})
-			.then((res) => {
-				return res;
 			})
 			.catch((e) => {
 				console.log("register error", e.response);
 				return e.response;
 			});
-		return response;
 	},
-	dev: async () => {
-		const response = await instance.get("/temp/register").then((res) => {
-			console.log("temp", res);
+	resign: async (email: string, pw: string) => {
+		return await instance
+			.post("/member/resign", {
+				userEmail: email,
+				password: pw,
+			})
+			.catch((e) => {
+				console.log("resign error", e.response);
+				return e.response;
+			});
+	},
+	getMyInfo: async (email: string) => {
+		return await instance.get(`/member/info?email=${email}`).catch((e) => {
+			console.log("getWord", e.response);
+			return e.response;
+		});
+	},
+
+	getWords: async (cnt: number) => {
+		return await instance.get(`/word/getWords?count=${cnt}`).catch((e) => {
+			console.log("getWord", e.response);
+			return e.response;
+		});
+	},
+	getWordByEng: async (word: string) => {
+		return await instance
+			.post(`/word/findByEnglish`, { english: word })
+			.catch((e) => {
+				console.log("getWord", e.response);
+				return e.response;
+			});
+	},
+
+	generateQuiz: async (cnt: number) => {
+		return await instance
+			.get(`/quiz/generateEnglishQuiz?count=${cnt}`)
+			.catch((e) => {
+				console.log("generateQuiz", e.response);
+				return e.response;
+			});
+	},
+	submitQuiz: async (obj: object) => {
+		return await instance.post("quiz/saveQuiz", obj).catch((e) => {
+			console.log("generateQuiz", e.response);
+			return e.response;
+		});
+	},
+
+	getQuizzes: async () => {
+		return await instance.get(`/quiz/findAllByUserId`).catch((e) => {
+			console.log("getQuizzes", e.response);
+			return e.response;
+		});
+	},
+	getQuiz: async (id: number) => {
+		return await instance.get(`/quiz/findById?id=${id}`).catch((e) => {
+			console.log("getQuiz", e.response);
+			return e.response;
 		});
 	},
 };
