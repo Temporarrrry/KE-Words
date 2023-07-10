87cfa07ff6988705ce6dc372d2e3ca2d1fdcfcc8
L-Hyun <dkh1001@outlook.kr>
2023-07-10 20:53:22 +0900

    Feat 연습하기 기능 추가 이후 라우팅 구조 변환

--- src/components/routers/RootRouter.tsx
+++ src/components/routers/RootRouter.tsx
@@ -3,18 +3,35 @@ import LoginPage from "../pages/LoginPage";
 import MainPage from "../pages/MainPage";
 import RegisterPage from "../pages/RegisterPage";
 import InfoPage from "../pages/InfoPage";
+import QuizListPage from "../pages/QuizListPage";
+import StudySelectPage from "../pages/StudySelectPage";
+import TestSelectPage from "../pages/TestSelectPage";
+import PracticeSelectPage from "../pages/PracticeSelectPage";
 
 type Props = {};
 
 function RootRouter({}: Props) {
-	return (
-		<Routes>
-			<Route path="/" element={<MainPage />} />
-			<Route path="/login" element={<LoginPage />} />
-			<Route path="/register" element={<RegisterPage />} />
-			<Route path="/info" element={<InfoPage />} />
-		</Routes>
-	);
+  return (
+    <Routes>
+      <Route path="/" element={<MainPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/info" element={<InfoPage />} />
+      <Route path="/study" element={<StudySelectPage />} />
+      <Route path="/study/word" element={<InfoPage />} />
+      <Route path="/study/sentence" element={<InfoPage />} />
+      <Route path="/practice" element={<PracticeSelectPage />} />
+      <Route path="/practice/word" element={<InfoPage />} />
+      <Route path="/practice/sentence" element={<InfoPage />} />
+      <Route path="/practice/order" element={<InfoPage />} />
+      <Route path="/practice/blank" element={<InfoPage />} />
+      <Route path="/test" element={<TestSelectPage />} />
+      <Route path="/test/word" element={<InfoPage />} />
+      <Route path="/test/sentence" element={<InfoPage />} />
+      <Route path="/test/order" element={<InfoPage />} />
+      <Route path="/test/blank" element={<InfoPage />} />
+    </Routes>
+  );
 }
 
 export default RootRouter;
